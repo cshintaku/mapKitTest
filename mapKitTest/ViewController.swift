8f5054f48db073fffd330ee83394cdd6af826d41
aki <aki@uncode.co.jp>
2019-04-17 10:24:51 +0900

    initial commit

---
+++ mapKitTest/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  mapKitTest
+//
+//  Created by aki on 2019/04/17.
+//  Copyright © 2019 uncode,Inc. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
