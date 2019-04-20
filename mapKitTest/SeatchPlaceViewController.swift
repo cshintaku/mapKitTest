88b84fd6db15c193768714356c8d1fc2f274228d
chi <chiyumyun@gmail.com>
2019-04-20 22:24:36 +0900

    目的地を設定し、所要時間を取得

---
+++ mapKitTest/SeatchPlaceViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SeatchPlaceViewController.swift
+//  mapKitTest
+//
+//  Created by aki on 2019/04/19.
+//  Copyright © 2019 uncode,Inc. All rights reserved.
+//
+
+import UIKit
+
+class SeatchPlaceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
